1ace3feb4a70ed913f4d77eed1122a93b5a79d6f
strikerkenpo <144185874+strikerkenpo@users.noreply.github.com>
2023-09-20 08:48:00 -0500

    Update NavBar.js

--- src/Pages/structure/NavBar.js
+++ src/Pages/structure/NavBar.js
@@ -1,1 +1,22 @@
 
+import { Link } from "react-router-dom";
+
+
+export const NavBar =() =>{
+
+        return(
+            <div>
+                <nav>
+                    <Link to="/">Home Page</Link>
+                    <Link to="/Login">Login</Link>
+                    <Link to="/About">About</Link>
+                 </nav>
+            </div>
+        
+        )
+}
+    
+        
+    
+    
+
