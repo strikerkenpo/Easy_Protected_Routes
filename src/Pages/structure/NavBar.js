d2c85085b16e5a95ae0eb88f29045b9d2f40a2b7
strikerkenpo <144185874+strikerkenpo@users.noreply.github.com>
2023-09-20 09:39:50 -0500

    Update NavBar.js

--- src/Pages/structure/NavBar.js
+++ src/Pages/structure/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 
 export const NavBar =() =>{
-        const img = ["./Pictures/autobot2.png"]
+       
         return(
             <div >
                 
@@ -12,7 +12,7 @@ export const NavBar =() =>{
                     <Link to="/" className="grid8">Home Page</Link>
                     <Link to="/Login" className="grid9">Login</Link>
                     <Link to="/About">About</Link>
-                    <img src={img} height={50} width={50} className="grid16" alt=""/>
+                   
                  </nav>
             </div>
         
