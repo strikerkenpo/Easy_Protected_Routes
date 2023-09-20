d4ac4f8d8d6b8261cd78e1a0a3b00e32fcf97ca8
strikerkenpo <144185874+strikerkenpo@users.noreply.github.com>
2023-09-20 09:40:24 -0500

    Update SecureNav.js

--- src/Pages/structure/SecureNav.js
+++ src/Pages/structure/SecureNav.js
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom"
 
 export const SecNav =() =>{
+    
     return(
         
-        <div>
-        <nav>
-            <Link to="/">Home Page</Link>
-            <Link to="/Login">Login</Link>
+        <div >
+        <nav className="navbar">
+            <Link to="/" className="grid8">Home Page</Link>
+            <Link to="/Login" className="grid9">Login</Link>
             <Link to="/About">About</Link>
             <Link to="/Account">Account</Link>
+           
         </nav>
     </div>
 
